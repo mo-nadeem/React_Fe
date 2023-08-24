8834499b2124f6ce1a7f223d181e76983601d119
manishkv2000@gmail.com <manishkv2000@gmail.com>
2023-08-24 15:07:15 +0530

    changes

--- src/pages/blogList/BlogList.js
+++ src/pages/blogList/BlogList.js
@@ -1,55 +1,115 @@
-import React from "react";
+import React, { useCallback, useEffect } from "react";
 import Homelayout from "../../components/Homelayout/Homelayout";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchHome } from "../../Api/action/HomeAction";
+import { Link } from "react-router-dom";
 
 const BlogList = () => {
+  const dispatch = useDispatch();
+
+  const { blog } = useSelector((state) => state.data);
+
+  const fetchHomedata = useCallback(() => {
+    dispatch(fetchHome());
+  }, [dispatch]);
+
+  useEffect(() => {
+    fetchHomedata();
+  }, [fetchHomedata]);
+
+  const blog1 = blog?.slice(0, 1) ?? [];
+  const blog2 = blog?.slice(1, 2) ?? [];
+  const blog3 = blog?.slice(2, 3) ?? [];
+  const blog4 = blog?.slice(3, 4) ?? [];
+  const blog5 = blog?.slice(0, 3) ?? [];
+
   return (
     <>
       <Homelayout>
         <section id="bloglist-head">
           <div className="midbox-inner  wiki-mk">
             <div className="bloglist">
-              <div className="bloglist-left">
-                <img src="images/2023/04/04/1.jpg" alt="" />
-                <div className="bloglist-box">
-                  <h2>Lorem ipsum dolor sit amet</h2>
-                  <p>
-                    Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut
-                    odit aut fugit, sed quia consequuntur ma Sed ut perspiciatis
-                    Nemo
-                  </p>
+              {blog1.map((e) => (
+                <div className="bloglist-left" key={e.id}>
+                  <img
+                    src={`${process.env.REACT_APP_BASE_URL}/blog/${e.icon}`}
+                    alt={e.name}
+                  />
+                  <div className="bloglist-box">
+                    <Link to={`/blog/${e.slug}`}>
+                      <h2>{e.name}</h2>
+                    </Link>
+
+                    {/* <p>
+                      Nemo enim ipsam voluptatem quia voluptas sit aspernatur
+                      aut odit aut fugit, sed quia consequuntur ma Sed ut
+                      perspiciatis Nemo
+                    </p> */}
+                    <div
+                      dangerouslySetInnerHTML={{ __html: e.short_description }}
+                    />
+                  </div>
                 </div>
-              </div>
+              ))}
 
               <div className="bloglist-right">
-                <div className="bloglist-box1">
-                  <img src="images/2023/04/04/2.jpg" alt="" />
-                  <div className="bloglist-box">
-                    <h2>
-                      Lorem ipsum dolor sit amet quia aspernatur aut odit aut
-                      fugit sed ut
-                    </h2>
+                {blog2.map((e) => (
+                  <div className="bloglist-box1" key={e.id}>
+                    <img
+                      src={`${process.env.REACT_APP_BASE_URL}/blog/${e.icon}`}
+                      alt={e.name}
+                    />
+                    <div className="bloglist-box">
+                      <Link to={`/blog/${e.slug}`}>
+                        <h2>{e.name}</h2>
+                      </Link>
+                      <div
+                        dangerouslySetInnerHTML={{
+                          __html: e.short_description,
+                        }}
+                      />
+                    </div>
                   </div>
-                </div>
+                ))}
                 <div className="bloglist-box2">
-                  <div className="bloglist-box3">
-                    <img src="images/2023/04/04/3.jpg" alt="" />
-                    <div className="bloglist-box">
-                      <h2>Lorem ipsum dolor sit amet quia</h2>
+                  {blog3.map((e) => (
+                    <div className="bloglist-box3" key={e.id}>
+                      <img
+                        src={`${process.env.REACT_APP_BASE_URL}/blog/${e.icon}`}
+                        alt={e.name}
+                      />
+                      <div className="bloglist-box">
+                        <Link to={`/blog/${e.slug}`}>
+                          <h2>{e.name}</h2>
+                        </Link>
+                        {/* <div
+                          dangerouslySetInnerHTML={{
+                            __html: e.short_description,
+                          }}
+                        /> */}
+                      </div>
                     </div>
-                  </div>
-                  <div className="bloglist-box4">
-                    <img src="images/2023/04/04/4.jpg" alt="" />
-                    <div className="bloglist-box">
-                      <h2>Lorem ipsum dolor sit amet quia</h2>
+                  ))}
+                  {blog4.map((e) => (
+                    <div className="bloglist-box4" key={e.id}>
+                      <img
+                        src={`${process.env.REACT_APP_BASE_URL}/blog/${e.icon}`}
+                        alt={e.name}
+                      />
+                      <div className="bloglist-box">
+                        <Link to={`/blog/${e.slug}`}>
+                          <h2>{e.name}</h2>
+                        </Link>
+                      </div>
                     </div>
-                  </div>
+                  ))}
                 </div>
               </div>
             </div>
           </div>
         </section>
 
-        <section className="bloglist-section">
+        {/* <section className="bloglist-section">
           <div className="midbox-inner  wiki-mk">
             <div className="bloglist-itembox">
               <div className="bloglist-itemleft">
@@ -239,7 +299,7 @@ const BlogList = () => {
               </div>
             </div>
           </div>
-        </section>
+        </section> */}
 
         <section className="bloglist-section">
           <div className="midbox-inner  wiki-mk">
@@ -247,19 +307,25 @@ const BlogList = () => {
               <div className="bloglist-itemleft">
                 <h3>Lorem ipsum dolor amet fugit</h3>
                 <div className="bloglist-itemmid">
-                  <div className="bloglist-item">
-                    <img src="images/2023/04/04/16.jpg" />
-                    <h4>
-                      Lorem ipsum dolor sit amet quia aspernatur odit sed
-                      consequntr
-                    </h4>
-                    <div className="blog-datalist">Feb 12, 2023</div>
-                    <p>
-                      Aliqua id fugiat nostrud irure ex duis ea quis id quis ad
-                      et. Sunt qui esse pariatur
-                    </p>
-                  </div>
-                  <div className="bloglist-item">
+                  {blog5.map((e) => (
+                    <div className="bloglist-item" key={e.id}>
+                      <img
+                        src={`${process.env.REACT_APP_BASE_URL}/blog/${e.icon}`}
+                        alt={e.name}
+                        style={{ width: "300px", height: "200px" }}
+                      />
+                      <Link to={`/blog/${e.slug}`}>
+                        <h4>{e.name}</h4>
+                      </Link>
+                      <div className="blog-datalist">Feb 12, 2023</div>
+                      <div
+                        dangerouslySetInnerHTML={{
+                          __html: e.short_description,
+                        }}
+                      />
+                    </div>
+                  ))}
+                  {/* <div className="bloglist-item">
                     <img src="images/2023/04/04/17.jpg" />
                     <h4>
                       Lorem ipsum dolor sit amet quia aspernatur odit sed
@@ -282,7 +348,7 @@ const BlogList = () => {
                       Aliqua id fugiat nostrud irure ex duis ea quis id quis ad
                       et. Sunt qui esse pariatur
                     </p>
-                  </div>
+                  </div> */}
                 </div>
 
                 <div className="bloglist-itembottom">
