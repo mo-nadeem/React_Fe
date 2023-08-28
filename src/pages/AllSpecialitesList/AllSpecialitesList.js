11eb96bca2f3a78c0020bcc4feb975d38856d60d
manishkv2000@gmail.com <manishkv2000@gmail.com>
2023-08-28 10:42:18 +0530

    dd

--- src/pages/AllSpecialitesList/AllSpecialitesList.js
+++ src/pages/AllSpecialitesList/AllSpecialitesList.js
@@ -23,8 +23,109 @@ const AllSpecialitesList = () => {
   return (
     <>
       <Homelayout>
-        {speciality.map((e) => {
-          // Filter gallery items that match the current hospital's id
+        <section id="medical-treatments">
+          <div class="midbox-inner  wiki-mk">
+            <h1>Medical Treatments</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore Ut enim ad minim veniam, quis
+              nostrud exercitation dolor sit
+            </p>
+
+            <div class="medical-treatments">
+              {speciality.map((e) => {
+                const galleryImages = treatment.filter(
+                  (gallery) => gallery.speciality_id === String(e.id)
+                );
+                return (
+                  <div class="medical-treatments-box" key={e.id}>
+                    <Link to={`/speciality/${e.slug}/${e.country}`}>
+                      <h2>{e.name}</h2>
+                    </Link>
+                    <ul>
+                      {galleryImages &&
+                        galleryImages.map((p) => (
+                          <Link
+                            to={`/treatment/${p.slug}/${p.country}`}
+                            key={p.id}
+                          >
+                            <li>{p.name}</li>
+                          </Link>
+                        ))}
+                    </ul>
+                  </div>
+                );
+              })}
+
+              {/* <div class="medical-treatments-box">
+                <h2>Neuro Surgery</h2>
+                <ul>
+                  <li> Brain Tumor </li>
+                  <li> Dystonia Parkinsons </li>
+                  <li> Interstim Therapy </li>
+                  <li> Spinal Cord Injury</li>
+                </ul>
+              </div>
+
+              <div class="medical-treatments-box">
+                <h2>Transplants</h2>
+                <ul>
+                  <li>Liver Transplant </li>
+                  <li> Kidney Transplant </li>
+                  <li> Heart Transplant </li>
+                  <li> Bone Marrow Transplant </li>
+                </ul>
+              </div>
+
+              <div class="medical-treatments-box">
+                <h2>Transplants</h2>
+                <ul>
+                  <li>Liver Transplant </li>
+                  <li> Kidney Transplant </li>
+                  <li> Heart Transplant </li>
+                  <li> Bone Marrow Transplant </li>
+                </ul>
+              </div>
+
+              <div class="medical-treatments-box">
+                <h2>Transplants</h2>
+                <ul>
+                  <li>Liver Transplant </li>
+                  <li> Kidney Transplant </li>
+                  <li> Heart Transplant </li>
+                  <li> Bone Marrow Transplant </li>
+                </ul>
+              </div>
+
+              <div class="medical-treatments-box">
+                <h2>Transplants</h2>
+                <ul>
+                  <li>Liver Transplant </li>
+                  <li> Kidney Transplant </li>
+                  <li> Heart Transplant </li>
+                  <li> Bone Marrow Transplant </li>
+                </ul>
+              </div> */}
+            </div>
+          </div>
+        </section>
+
+        <section class="treatment-marquee" id="category-marquee">
+          <div class="marquee-wrapper">
+            <div class="marquee" style={{ animationDuration: "21s" }}>
+              <div class="Marquee-tag"> 100+ Surgeries </div>
+              <div class="Marquee-tag"> 95% Success Rate </div>
+              <div class="Marquee-tag"> 4000+ Top Doctors </div>
+              <div class="Marquee-tag"> 1000+ Top Hospital </div>
+              <div class="Marquee-tag"> 100+ Surgeries </div>
+              <div class="Marquee-tag"> 95% Success Rate </div>
+              <div class="Marquee-tag"> 4000+ Top Doctors </div>
+              <div class="Marquee-tag"> 1000+ Top Hospital </div>
+            </div>
+          </div>
+        </section>
+        {/* {speciality.map((e) => {
+       
           const galleryImages = treatment.filter(
             (gallery) => gallery.speciality_id === String(e.id)
           );
@@ -44,7 +145,7 @@ const AllSpecialitesList = () => {
               </ul>
             </div>
           );
-        })}
+        })} */}
       </Homelayout>
     </>
   );
