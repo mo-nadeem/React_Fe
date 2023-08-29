ef87fd35fc0c36bf9f7a9754a4817eff4d6ba4af
manishkv2000@gmail.com <manishkv2000@gmail.com>
2023-08-29 10:27:41 +0530

    chnages

--- src/pages/questionAns/QuestionAns.js
+++ src/pages/questionAns/QuestionAns.js
@@ -5,6 +5,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { fetchHome } from "../../Api/action/HomeAction";
 import { AiOutlineHeart } from "react-icons/ai";
 import { AiTwotoneHeart } from "react-icons/ai";
+import arrowCIcon from "../../assests/images/2023/01/arrow-c.png";
 import axios from "axios";
 const QuestionAns = () => {
   const dispatch = useDispatch();
@@ -106,6 +107,52 @@ const QuestionAns = () => {
       .finally(() => {
         // Set loading back to false after the API call is complete
         setIsLoading(false);
+      });
+  };
+
+
+  // Assistance form post 
+  
+  const [name1, setName1] = useState("");
+  const [pcode1, setPcode1] = useState("");
+  const [phone1, setPhone1] = useState("");
+  const [email1, setEmail1] = useState("");
+  const [query1, setQuery1] = useState("");
+
+  const [isLoading1, setIsLoading1] = useState(false);
+
+  const handleFormSubmit1 = (event) => {
+    event.preventDefault();
+
+    // Create the data object to be sent in the API request
+    const data = {
+      name: name1,
+      phone_code: pcode1,
+      phone: phone1,
+      email: email1,
+      messages: query1,
+    };
+
+    // Replace 'YOUR_API_ENDPOINT' with your actual API endpoint
+    const apiEndpoint = `${process.env.REACT_APP_BASE_URL}/api/free_consultants`;
+
+    setIsLoading1(true);
+
+    // Make the API call
+    axios
+      .post(apiEndpoint, data)
+      .then((response) => {
+        // Handle the API response here if needed
+        console.log(response);
+        alert("questions is susscefull submitted");
+      })
+      .catch((error) => {
+        // Handle any errors that occurred during the API call
+        console.error("Error:", error);
+      })
+      .finally(() => {
+        // Set loading back to false after the API call is complete
+        setIsLoading1(false);
       });
   };
   return (
@@ -750,105 +797,120 @@ const QuestionAns = () => {
                 <div className="treatment-right">
                   <h2>Need Assistance?</h2>
 
-                  <div className="treatment-form">
-                    <div className="inputbox">
-                      <label>Name</label>
-                      <input
-                        type="text"
-                        placeholder=""
-                        name="name"
-                        required=""
-                      />
+                  <form onSubmit={handleFormSubmit1}>
+                    <div className="treatment-form">
+                      <div className="inputbox">
+                        <label>Name</label>
+                        <input
+                          type="text"
+                          placeholder=""
+                          name="name"
+                          required
+                          value={name1}
+                          onChange={(e) => setName1(e.target.value)}
+                        />
+                      </div>
                     </div>
-                  </div>
 
-                  <div className="treatment-form">
-                    <div className="inputbox">
-                      <label>Phone</label>
-                      <div className="phone-form">
-                        <div className="phone-box1">
-                          <select
-                            aria-label="Sort dropdown"
-                            className="phone-dropdown"
-                          >
-                            <option value="">Choose Code</option>
-                            <option value="1">UK (+44)</option>
-                            <option value="213">Algeria (+213)</option>
-                            <option value="376">Andorra (+376)</option>
-                            <option value="244">Angola (+244)</option>
-                            <option value="1264">Anguilla (+1264)</option>
-                            <option value="1268">
-                              Antigua &amp; Barbuda (+1268)
-                            </option>
-                            <option value="54">Argentina (+54)</option>
-                            <option value="374">Armenia (+374)</option>
-                            <option value="297">Aruba (+297)</option>
-                            <option value="61">Australia (+61)</option>
-                            <option value="43">Austria (+43)</option>
-                            <option value="994">Azerbaijan (+994)</option>
-                            <option value="1242">Bahamas (+1242)</option>
-                            <option value="973">Bahrain (+973)</option>
-                            <option value="880">Bangladesh (+880)</option>
-                            <option value="1246">Barbados (+1246)</option>
-                            <option value="375">Belarus (+375)</option>
-                            <option value="32">Belgium (+32)</option>
-                            <option value="501">Belize (+501)</option>
-                            <option value="229">Benin (+229)</option>
-                            <option value="1441">Bermuda (+1441)</option>
-                            <option value="975">Bhutan (+975)</option>
-                            <option value="591">Bolivia (+591)</option>
-                            <option value="387">
-                              Bosnia Herzegovina (+387)
-                            </option>
-                            <option value="267">Botswana (+267)</option>
-                            <option value="55">Brazil (+55)</option>
-                            <option value="673">Brunei (+673)</option>
-                            <option value="359">Bulgaria (+359)</option>
-                            <option value="226">Burkina Faso (+226)</option>
-                            <option value="257">Burundi (+257)</option>
-                            <option value="855">Cambodia (+855)</option>
-                          </select>
-                        </div>
-                        <div className="phone-box2">
-                          <input
-                            type="text"
-                            placeholder=""
-                            name="name"
-                            required=""
-                          />
+                    <div className="treatment-form">
+                      <div className="inputbox">
+                        <label>Phone</label>
+                        <div className="phone-form">
+                          <div className="phone-box1">
+                            <select
+                              aria-label="Sort dropdown"
+                              className="phone-dropdown"
+                              value={pcode1}
+                              onChange={(e) => setPcode1(e.target.value)}
+                            >
+                              <option value="">Choose Code</option>
+                              <option value="+91">India (+91)</option>
+                              <option value="1">UK (+44)</option>
+                              <option value="213">Algeria (+213)</option>
+                              <option value="376">Andorra (+376)</option>
+                              <option value="244">Angola (+244)</option>
+                              <option value="1264">Anguilla (+1264)</option>
+                              <option value="1268">
+                                Antigua &amp; Barbuda (+1268)
+                              </option>
+                              <option value="54">Argentina (+54)</option>
+                              <option value="374">Armenia (+374)</option>
+                              <option value="297">Aruba (+297)</option>
+                              <option value="61">Australia (+61)</option>
+                              <option value="43">Austria (+43)</option>
+                              <option value="994">Azerbaijan (+994)</option>
+                              <option value="1242">Bahamas (+1242)</option>
+                              <option value="973">Bahrain (+973)</option>
+                              <option value="880">Bangladesh (+880)</option>
+                              <option value="1246">Barbados (+1246)</option>
+                              <option value="375">Belarus (+375)</option>
+                              <option value="32">Belgium (+32)</option>
+                              <option value="501">Belize (+501)</option>
+                              <option value="229">Benin (+229)</option>
+                              <option value="1441">Bermuda (+1441)</option>
+                              <option value="975">Bhutan (+975)</option>
+                              <option value="591">Bolivia (+591)</option>
+                              <option value="387">
+                                Bosnia Herzegovina (+387)
+                              </option>
+                              <option value="267">Botswana (+267)</option>
+                              <option value="55">Brazil (+55)</option>
+                              <option value="673">Brunei (+673)</option>
+                              <option value="359">Bulgaria (+359)</option>
+                              <option value="226">Burkina Faso (+226)</option>
+                              <option value="257">Burundi (+257)</option>
+                              <option value="855">Cambodia (+855)</option>
+                            </select>
+                          </div>
+                          <div className="phone-box2">
+                            <input
+                              type="text"
+                              placeholder=""
+                              name="name"
+                              required
+                              value={phone1}
+                              onChange={(e) => setPhone1(e.target.value)}
+                            />
+                          </div>
                         </div>
                       </div>
                     </div>
-                  </div>
 
-                  <div className="treatment-form">
-                    <div className="inputbox">
-                      <label>Email</label>
-                      <input
-                        type="text"
-                        placeholder=""
-                        name="name"
-                        required=""
-                      />
+                    <div className="treatment-form">
+                      <div className="inputbox">
+                        <label>Email</label>
+                        <input
+                          type="text"
+                          placeholder=""
+                          name="name"
+                          required
+                          value={email1}
+                          onChange={(e) => setEmail1(e.target.value)}
+                        />
+                      </div>
                     </div>
-                  </div>
 
-                  <div className="treatment-form">
-                    <div className="inputbox">
-                      <label>Your Query</label>
-                      <textarea
-                        className="querybox"
-                        type="textarea"
-                        name="query"
-                        placeholder=""
-                        rows="2"
-                      ></textarea>
+                    <div className="treatment-form">
+                      <div className="inputbox">
+                        <label>Your Query</label>
+                        <textarea
+                          className="querybox"
+                          type="textarea"
+                          name="query"
+                          placeholder=""
+                          rows="2"
+                          value={query1}
+                          onChange={(e) => setQuery1(e.target.value)}
+                        ></textarea>
+                      </div>
                     </div>
-                  </div>
 
-                  <button type="submit" name="en" className="home-button">
-                    Submit Now <img src="images/2023/01/arrow-c.png" alt="" />
-                  </button>
+                    <button type="submit" name="en" className="home-button">
+                      {" "}
+                      {isLoading1 ? "Submitting..." : "Submit Now"}
+                      <img src={arrowCIcon} alt="arrow-Icon" />
+                    </button>
+                  </form>
                 </div>
               </div>
             </div>
